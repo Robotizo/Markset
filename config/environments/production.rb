b96b0abced5868a67acc6322ebb36ccdaa030d5a
Brandon Caiza <brandoncaiza97@hotmail.com>
2017-11-16 00:55:44 -0800

    Back again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -60,11 +60,15 @@ Rails.application.configure do
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.default_url_options = { :host => 'markset.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
- 
+  config.action_mailer.perform_deliveries = true  
+  config.action_mailer.raise_delivery_errors = false  
+  config.action_mailer.default :charset => "utf-8" 
   config.action_mailer.smtp_settings = {
     address:              'smtp.mail.com',
     port:                 587,
+    domain:               'markset.herokuapp.com',
     user_name:            'geniusrobotizo@gmail.com',
     password:             '@Brandoncaizamanu7@!',
     authentication:       'plain',
