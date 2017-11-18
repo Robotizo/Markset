ea15f4047f9203027798e2199d29f5dce1798bc3
Brandon Caiza <brandoncaiza97@hotmail.com>
2017-11-18 12:19:17 -0800

    CSS Edits

--- app/models/page.rb
+++ app/models/page.rb
@@ -29,6 +29,7 @@ class Page < ApplicationRecord
 
 	has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
 	has_many :followers, through: :passive_relationships, source: :follower
+
 end
 
 
