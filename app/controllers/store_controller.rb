e211201826d6f8baa327ce2264c307f5e8064418
Brandon Caiza <brandoncaiza97@hotmail.com>
2018-01-30 00:23:13 -0800

    Front page reworking 1

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -5,6 +5,7 @@ class StoreController < ApplicationController
   	@products = Product.all.order("created_at DESC")
   	followingIds = current_user.following.map(&:id)
 	@posts = Post.where(page_id: followingIds).order("created_at DESC")
+	@initialPosts = Post.all.order("created_at DESC")
   	@cart = current_cart
   	@pages = Page.all.order("created_at DESC")
   	@categories = Category.all.order("created_at DESC")
