24a00a32f9259f5f046e2026827104c79e014f00
Brandon Caiza <brandoncaiza97@hotmail.com>
2018-02-19 20:22:50 -0800

    First iteration of folliwing FP

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -2,13 +2,13 @@ class StoreController < ApplicationController
   skip_before_action :authorize
 
   def index
-  	@products = Product.all.order("created_at DESC")
+    @products = Product.all.order("created_at DESC")
     followingIds = current_user.following.map(&:id)
-	  @posts = Post.where(page_id: followingIds).order("created_at DESC")
-	  @initialPosts = Post.all.order("created_at DESC")
-  	@cart = current_cart
-  	@pages = Page.all.order("created_at DESC")
-  	@categories = Category.all.order("created_at DESC")
+    @pagesFollowing = Page.find(params = followingIds).reverse!
+    @initialPosts = Post.all.order("created_at DESC")
+    @cart = current_cart
+    @pages = Page.all.order("created_at DESC")
+    @categories = Category.all.order("created_at DESC")
     @notFollowingPages = Page.order("RANDOM()")
   end
 end
