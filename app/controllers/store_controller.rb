1880aaf75871618ce1a0352b5acad4b502840cb7
Brandon Caiza <brandoncaiza97@hotmail.com>
2018-02-20 16:41:00 -0800

    Still in the process FPF

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -5,6 +5,7 @@ class StoreController < ApplicationController
     @products = Product.all.order("created_at DESC")
     followingIds = current_user.following.map(&:id)
     @pagesFollowing = Page.find(params = followingIds).reverse!
+    @pagesNotFollowing = Page.where.not(id: followingIds)
     @initialPosts = Post.all.order("created_at DESC")
     @cart = current_cart
     @pages = Page.all.order("created_at DESC")
