7255ace925d5a6a057a18800e854e5710263c482
Brandon Caiza <brandoncaiza97@hotmail.com>
2018-04-22 17:38:42 -0700

    Solving

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,10 @@ module ApplicationHelper
 		end
 		content_tag("div", attributes, &block)
 	end
+
+	def stripe_url
+		"https://dashboard.stripe.com/oauth/authorize?response_type=code&client_id=ca_CiwROK7GO3kkQW5cJzC144PZqNmaTPC9&scope=read_write"
+	end
+
+
 end
